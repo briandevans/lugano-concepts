7e2e234ab65d89873c452708e47bb15d44b4271c
Brian D. Evans <bdegithub@pm.me>
2026-09-22 08:39:24 -0700

    Tighten and center crossed-out headline annotation

--- homepage-router.js
+++ homepage-router.js
@@ -34,7 +34,7 @@
         },
         {
           id: "lugano-panorama-style-css",
-          href: "/concepts/longbow/opening/panorama/style.css?v=hero-20260922-14",
+          href: "/concepts/longbow/opening/panorama/style.css?v=hero-20260922-15",
         },
         {
           id: "lugano-panorama-map-css",
